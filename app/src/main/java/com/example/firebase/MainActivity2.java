081f205d02fe13b24f2d74197f0865236af447c5
DevLaukey <devlaukeymwaura@gmail.com>
2023-02-16 13:30:08 +0300

    Complete

--- app/src/main/java/com/example/firebase/MainActivity2.java
+++ app/src/main/java/com/example/firebase/MainActivity2.java
@@ -1,17 +1,17 @@
 package com.example.firebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,54 +23,26 @@ import java.util.Map;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    public ListView listView;
+    private ListView listView;
+
+    // creating a new array list.
+    ArrayList<String> arrayList;
+
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         listView = (ListView)findViewById(R.id.listViews);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
+
+        arrayList =  new ArrayList<String>();
 
 
-
-        ArrayList<String> arrayList = new ArrayList<>();
-        myRef.child("user").addListenerForSingleValueEvent(new ValueEventListener(){
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    Map<String, Object> map = (Map<String, Object>)  dataSnapshot.getValue();
-
-                    String  name = (String) map.get("username");
-                    String  pass  = (String) map.get("password");
-                    Toast.makeText(MainActivity2.this, "The user clicked is: "+""+ pass, Toast.LENGTH_SHORT).show();
-
-                    arrayList.add(name + pass);
-
-                }
-            }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Getting Post failed, log a message
-            }
-        });
-        arrayList.add("Username :" + "Password :");
-        arrayList.add("Username :" + "Password :");
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
-
-        listView.setAdapter(arrayAdapter);
 
         Button button2 = findViewById(R.id.back_button);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity2.this, "The user clicked is: "+ i + "" +arrayList.get(i).toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
+
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,7 +52,81 @@ public class MainActivity2 extends AppCompatActivity {
                         intent
                 );
             }
+
         });
+        initializeListView();
+
 
     }
+    private void initializeListView() {
+        // creating a new array adapter for our list view.
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, arrayList);
+
+        // below line is used for getting reference
+        // of our Firebase Database.
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        // in below line we are calling method for add child event
+        // listener to get the child of our database.
+
+        int count = 1;
+
+        while (count < 6){
+            int finalCount = count;
+            reference.child("user"+ count).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Map<String, Object> map = (Map<String, Object>)  snapshot.getValue();
+
+                    arrayList.add(" " + finalCount + " ."+ "Username : " + map.get("username") + "         " + "Password : " + " " +map.get("password"));
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+            count++ ;
+
+        }
+
+        // below line is used for setting
+        // an adapter to our list view.
+        listView.setAdapter(adapter);
+    }
+
+
 }
+
+
+//        myRef.child("user").addListenerForSingleValueEvent(new ValueEventListener(){
+//@Override
+//public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//        if(dataSnapshot.exists()){
+//        Map<String, Object> map = (Map<String, Object>)  dataSnapshot.getValue();
+//
+//        String  name = (String) map.get("username");
+//        String  pass  = (String) map.get("password");
+//        arrayList.add("Casdcas");
+//
+//        arrayAdapter.notifyDataSetChanged();
+//        }
+//        }
+//
+//
+//@Override
+//public void onCancelled(@NonNull DatabaseError databaseError) {
+//        // Getting Post failed, log a message
+//        Toast.makeText(MainActivity2.this, "Fail to load data..", Toast.LENGTH_SHORT).show();
+//
+//        }
+//        });
+//        listView.setAdapter(arrayAdapter);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//@Override
+//public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//        Toast.makeText(MainActivity2.this, "The user clicked is: "+ i + "" +arrayList.get(i).toString(), Toast.LENGTH_SHORT).show();
+//        }
+//        });
